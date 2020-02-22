984484242c8a325b55dd3067bcc76a338b82212f
ncs2738 <ncs2738@rit.edu>
2020-02-22 02:54:04 -0500

    Fixed

--- src/responses.js
+++ src/responses.js
@@ -1,38 +1,33 @@
-const fs = require('fs'); 
+const fs = require('fs');
+
 const index = fs.readFileSync(`${__dirname}/../client/login.html`);
 const css = fs.readFileSync(`${__dirname}/../css/styles.css`);
 
-const getResponse = (request, response, statusCode, contentType, content) => 
-{
+const getResponse = (request, response, statusCode, contentType, content) => {
   response.writeHead(statusCode, { 'Content-Type': contentType });
   response.write(content);
   response.end();
 };
 
-const getIndex = (request, response) =>
-{
-    getResponse(request, response, 200, 'text/html', index)
+const getIndex = (request, response) => {
+  getResponse(request, response, 200, 'text/html', index);
 };
 
-const getCSS = (request, response) =>
-{
-    getResponse(request, response, 200, 'text/css', css)
+const getCSS = (request, response) => {
+  getResponse(request, response, 200, 'text/css', css);
 };
 
-const notFound = (request, response, acceptedTypes) =>
-{
-    const responseObj = 
-    {
-        message: 'The page you are looking for was not found.',
-        id: 'notFound',
-    };
+const notFound = (request, response, acceptedTypes) => {
+  const responseObj = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
 
-    sendResponse(request, response, acceptedTypes, responseObj, 404);
+  getResponse(request, response, acceptedTypes, responseObj, 404);
 };
 
-module.exports = 
-{
-    getIndex,
-    getCSS,
-    notFound,
+module.exports = {
+  getIndex,
+  getCSS,
+  notFound,
 };
--- src/server.js
+++ src/server.js
@@ -7,22 +7,21 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const urlStruct = {
   '/': responseHandler.getIndex,
   '/styles.css': responseHandler.getCSS,
-  '/notFound' : responseHandler.notFound,
+  '/notFound': responseHandler.notFound,
   index: responseHandler.getIndex,
   notFound: responseHandler.notFound,
 };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    const acceptedTypes = request.headers.accept.split(',');
+  const parsedUrl = url.parse(request.url);
+  const acceptedTypes = request.headers.accept.split(',');
 
-console.log(urlStruct[parsedUrl.pathname]);
+  console.log(urlStruct[parsedUrl.pathname]);
 
-    if (urlStruct[parsedUrl.pathname]) 
-    {
-      urlStruct[parsedUrl.pathname](request, response, acceptedTypes, request.url);
-    } 
-  };
-  http.createServer(onRequest).listen(port);
-  
-  console.log(`Listening on 127.0.0.1: ${port}`);
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, request.url);
+  }
+};
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
